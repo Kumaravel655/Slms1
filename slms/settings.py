260084c2434c2049362c357b256de03c01e026ff
Kumaravel <kumravelkuma07@gmail.com>
2024-04-27 00:07:12 +0530

    allow

--- slms/settings.py
+++ slms/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8j+nu3y0_ab%bhda_(x3=8&!a+s^!czew#-b2m#1m(-g#$uaya
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['kumaravelguna.pythonanywhere.com']
 
 
 # Application definition
